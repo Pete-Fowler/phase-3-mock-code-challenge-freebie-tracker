c7b563df2a3523ff9005ef44d5ebb9e6da550d42
Pete Fowler <fowler.pete@gmail.com>
2022-09-27 14:07:16 -0600

    Add solution

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,26 @@
 class Company < ActiveRecord::Base
   has_many :freebies
   has_many :devs, through: :freebies
+
+  def freebies
+    Freebie.where(company_id: self.id)
+  end
+
+  def devs
+    self.freebies.map do |f|
+      Dev.find(f.dev_id)
+    end
+  end
+
+  def give_freebie(dev, item, value)
+    Freebie.create(item_name: item, value: value, dev_id: dev.id, company_id: self.id)
+  end
+
+  def self.oldest_company
+    year = Company.minimum(:founding_year)
+    Company.find_by(founding_year: year)
+  end
+
+
+
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,4 +1,28 @@
 class Dev < ActiveRecord::Base
   has_many :freebies
   has_many :companies, through: :freebies
+
+
+  # def freebies
+  #   Freebie.where(dev_id: self.id)
+
+  # end
+
+  # def companies
+  #   self.freebies.map do |f|
+  #     Company.find(f.company_id)
+  #   end
+  # end
+
+
+  def received_one?(item)
+    self.freebies.any? do |freebie| 
+      freebie.item_name == item__name
+  end
+
+
+  def give_away(freebie, dev)
+    freebie.update(dev: dev) unless freebie.dev != self
+    end 
+  end 
 end
