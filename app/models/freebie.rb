e24d38dd0a17531ba981dcc45e2493b76cb9d972
Pete Fowler <fowler.pete@gmail.com>
2022-09-27 12:31:04 -0600

    Add print_details

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -5,4 +5,13 @@ class Freebie < ActiveRecord::Base
   def dev 
     Dev.find(self.dev_id)  
   end
+
+  def company 
+    Company.find(self.company_id)
+  end 
+
+  def print_details 
+    self.dev.name + ' owns a ' + self.item_name + ' from ' + self.company.name
+  end 
+
 end
